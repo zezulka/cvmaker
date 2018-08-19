48cc2f04c45410c54c4ab6c1672f0977fd3b5950
Miloslav Žežulka <zezulka95@gmail.com>
2018-08-19 19:14:26 +0200

    Refactoring - bring all font related info into one struct.

--- src/renderer.rs
+++ src/renderer.rs
@@ -15,6 +15,29 @@ struct Resolution {
     height : f32
 }
 
+// A simple wrapper around cursive structs.
+struct Font {
+    font : BuiltinFont,
+    font_bold : BuiltinFont,
+    fsize : f32,
+}
+
+impl Font {
+    pub fn new(font : BuiltinFont) -> Font {
+        Font { font, font_bold : Self::bold_font_factory(&font), fsize : 14.0 }
+    }
+
+    fn bold_font_factory(font : &BuiltinFont) -> BuiltinFont {
+        use self::BuiltinFont::*;
+        match font {
+            Courier => Courier_Bold,
+            Helvetica => Helvetica_Bold,
+            Times_Roman => Times_Bold,
+            _ => panic!("Unsupported font!"),
+        }
+    }
+}
+
 impl Resolution {
     // Respect the ratio specified by ISO 216
     // https://www.cl.cam.ac.uk/~mgk25/iso-paper.html
@@ -63,50 +86,54 @@ fn black_rectangle_white_text(c : &mut Canvas, text : &str, fref : &FontRef, fsi
     })
 }
 
-fn render_basic_info(c : &mut Canvas, Resolution {width, height} : &Resolution, fref : BuiltinFont, fref_bold : BuiltinFont, fsize : f32,
+fn render_basic_info(c : &mut Canvas, Resolution {width, height} : &Resolution, font : &Font,
                      BasicInfo {name, surname, dob, contacts} : &BasicInfo) -> IoRes<()> {
     c.set_stroke_color(Color::rgb(0,0,0))?;
     let name_surname = name.to_string() + " " + surname;
-    c.center_text(*width * 0.25, *height - 60.0, fref_bold, fsize * 2.0, &name_surname)?;
+    c.center_text(*width * 0.25, *height - 60.0, font.font_bold, font.fsize * 2.0, &name_surname)?;
     c.line(*width * 0.25, *height - 90.0, *width, *height - 90.0);
     c.stroke()?;
     let mut offset = *height - 150.0;
     contacts.iter().for_each(|contact| {
-        c.right_text(*width * 0.75, offset, fref_bold, fsize, &format!("{:#}", contact));
+        c.right_text(*width * 0.75, offset, font.font_bold, font.fsize, &format!("{:#}", contact));
         offset -= 30.0;
     });
     Ok(())
 }
 
-fn render_experience(cv : &mut Canvas, data : &Vec<Experience>) -> IoRes<()> {
+fn render_experience(c : &mut Canvas, data : &Vec<Experience>, font : &Font) -> IoRes<()> {
+    //black_rectangle_white_text(c, "Experience", );
     Ok(())
 }
 
-fn render_education(cv : &mut Canvas, data : &Vec<Education>) -> IoRes<()> {
+fn render_education(cv : &mut Canvas, data : &Vec<Education>, font : &Font) -> IoRes<()> {
+    //black_rectangle_white_text(c);
     Ok(())
 }
 
-fn render_languages(cv : &mut Canvas, data : &Vec<Lang>) -> IoRes<()> {
+fn render_languages(cv : &mut Canvas, data : &Vec<Lang>, font : &Font) -> IoRes<()> {
+    //black_rectangle_white_text(c);
     Ok(())
 }
 
-//TODO : linewrapper, maybe add more structs which would improve the library composition a bit better.
+//TODO : maybe add more structs which would improve the library usage.
 pub fn render_pdf(cv : &CV) -> Result<(), String> {
+    // Leave the parameters hardwired for now.
     let res = Resolution::new_a4(1000.0);
     let Resolution {width, height} = res;
-
+    let font = Font::new(BuiltinFont::Times_Roman);
     let mut document = Pdf::create("/tmp/text.pdf").unwrap();
     let lorem_ipsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
     document.set_title("Foo bar");
     document.render_page(width, height, |c| {
-            let font = BuiltinFont::Times_Roman;
-            let font_ref = c.get_font(font);
-            render_basic_info(c, &res, font, BuiltinFont::Times_Bold, 14.0, &cv.basic)?;
-            render_experience(c, &cv.experience)?;
-            render_education(c, &cv.education)?;
-            render_languages(c, &cv.languages)?;
+            //let font_ref = c.get_font(font);
+            render_basic_info(c, &res, &font, &cv.basic)?;
+            //render_experience(c, &cv.experience)?;
+            render_education(c, &cv.education, &font)?;
+            render_languages(c, &cv.languages, &font)?;
             c.text(|t| {
-                t.set_font(&font_ref, 14.0)?;
+                //t.set_font(&font_ref, 14.0)?;
                 t.set_leading(18.0)?;
                 t.pos(10.0, 300.0)?;
                 wrap_line(t, 50, lorem_ipsum)
